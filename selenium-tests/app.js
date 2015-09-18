63cf269678b10512e1c66b3a8c115129e60be706
Delveri Munang <chickdelveri@gmail.com>
2015-09-18 15:08:06 +0100

    Added wrong password Login Test

--- selenium-tests/app.js
+++ selenium-tests/app.js
@@ -8,7 +8,7 @@ var driver = new webdriver.Builder()
 
 driver.get('http://localhost:9000');
 driver.findElement(By.name('pma_username')).sendKeys('root');
-driver.findElement(By.name('pma_password')).sendKeys('root1');
+driver.findElement(By.name('pma_password')).sendKeys('root');
 driver.findElement(By.id('input_go')).click();
 driver.wait(until.titleIs('webdriver - Google Search'), 9000);
 // driver.quit();
