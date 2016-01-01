cb0bef6ccc116a7e968c21b261ae5e9b8dc060d8
Delveri Munang <chickdelveri@gmail.com>
2016-01-02 00:18:44 +0100

    Added more fail test cases

--- test/test-math.js
+++ test/test-math.js
@@ -26,3 +26,14 @@ exports.test_add = function(test){
 	test.equals(math.add(1,3),5);
 	test.done();
 };
+
+
+exports.test_subtract = function(test){
+	test.equals(math.subtract(5,5),1);
+	test.done();
+};
+
+exports.test_modulo = function(test){
+	test.equals(math.modulo(5,5),0);
+	test.done();
+};
