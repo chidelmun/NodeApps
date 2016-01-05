8c0eb32717885d9aca28963999d93e01f99840f2
Delveri Munang <chickdelveri@gmail.com>
2016-01-05 23:39:43 +0100

    Using node and mysql::express

---
+++ mysql.js
@@ -0,0 +1,29 @@
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'node'
+});
+var app = express();
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+app.get("/",function(req,res){
+connection.query('SELECT * from users LIMIT 2', function(err, rows, fields) {
+connection.end();
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
+
+app.listen(7000);
