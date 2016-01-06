7fdf6c2a6f5b1036ea1196508d22cb1abc04955f
Delveri Munang <chickdelveri@gmail.com>
2016-01-06 18:40:59 +0100

    request handler interface

--- REST.js
+++ REST.js
@@ -1,18 +1,39 @@
 var http = require('http')
 var PORT = 9005
 
-function handle_GET_request(req,res){
+function handle_GET_request(res){
 	console.log("GET request received")
 }
 
-function handle_POST_request(req,res){
+function handle_POST_request(res){
 	console.log("POST request received")
 }
 
-function handle_PUT_request(req,res){
+function handle_PUT_request(res){
 	console.log("PUT request received")
 }
 
-function handle_DELETE_request(req,res){
+function handle_DELETE_request(res){
 	console.log("DELETE request received")
+}
+
+
+function request_handler(req,res){
+	switch(req.method){
+		case 'GET':
+			handle_GET_request(res);
+			break;
+
+		case 'POST':
+			handle_POST_request(res);
+			break;
+
+		case 'PUT':
+			handle_PUT_request(res);
+			break;
+		case 'DELETE':
+			handle_DELETE_request(res);
+			break;
+		
+	}
 }
