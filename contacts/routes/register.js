6b42460bc1f63e47edffd75594f7367614645572
Delveri Munang <chickdelveri@gmail.com>
2016-01-08 10:59:32 +0100

    Fetch device mac address for use at login

--- contacts/routes/register.js
+++ contacts/routes/register.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Signup Page' });
+  res.render('register', { title: 'Signup Page',mac : os.networkInterfaces().en1[0].mac.toString()  });
 });
 
 module.exports = router;
