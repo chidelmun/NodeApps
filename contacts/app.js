a64fec73436a7187e8183bc93a298d1893c76e68
Delveri Munang <chickdelveri@gmail.com>
2016-01-08 10:47:07 +0100

    Fixed login page route typo

--- contacts/app.js
+++ contacts/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/contacts',contacts);
-app.use('login',login);
+app.use('/login',login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
