747419526f084c3bbad4919570b67257f8007b0a
Delveri Munang <chickdelveri@gmail.com>
2016-01-08 10:53:16 +0100

    Added route for registration page

--- contacts/app.js
+++ contacts/app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var contacts = require('./routes/contacts')
 var login = require('./routes/login')
+var register = require('./routes/register')
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/contacts',contacts);
 app.use('/login',login);
+app.use('/register',register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
