e746599d7824fd423a81be41698d4a4e612736ee
Delveri Munang <chickdelveri@gmail.com>
2016-03-11 18:50:05 +0100

    Added support for #ublab logs

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var http = require('irc')
+var irc = require('irc')
 
 var client = new irc.Client('irc.freenode.net', 'kankaka', {
     channels: ['#ublab'],
+});
+
+client.addListener('message', function (from, to, message) {
+    console.log(from + ' => ' + to + ': ' + message);
+});
+
+client.addListener('pm', function (from, message) {
+    console.log(from + ' => ME: ' + message);
+});
+
+client.addListener('message#yourchannel', function (from, message) {
+    console.log(from + ' => #yourchannel: ' + message);
 });
