c60501a0c2332cc29bd6ab7f3d76d899360f7d1e
Delveri Munang <chickdelveri@gmail.com>
2016-03-27 17:37:18 +0100

    Enhanced tcp server

--- filename.js
+++ filename.js
@@ -1,10 +1,16 @@
 var tcp = require('net');
 
 tcp.createServer((socket)=>{
+	timeout = 5000;
+	socket.setTimeout(timeout);
+	socket.on('timeout', ()=>{
+		socket.write("Connection Timeout... Bye");
+		socket.end();
+	});
 	socket.write("Welcome\n");
 
 	socket.on('data', (data)=>{
 		console.log(data.toString());
 	})
 
-}).listen(8882);
+}).listen(8885);
