ef7892bee58d0fd7698228e840a2198590bc4515
Delveri Munang <chickdelveri@gmail.com>
2016-03-27 10:02:48 +0100

    testing respose status codes

--- filename.js
+++ filename.js
@@ -1,10 +1,21 @@
 var http = require('http');
 PORT = 8888;
 
-http.createServer(function(req,res){
-	res.writeHead(200,{"content-Type":"text/html"});
+app = http.createServer(function(req,res){
+	res.writeHead(200,{"content-Type":"text/html",
+						'content-length':'123',
+						'connection':'keep-alive'});
 	res.write("Hello World \n");
-	
+	if(res.statusCode != 200){
+		console.log("Bad Response");
+	}else
+	console.log("Valid request");
+
 }).listen(PORT);
+
+app.on('connect', function(req,res,head){
+	console.log("Client connected \n");
+});
+
 console.log(__filename);
 console.log("Server running on port " + PORT);
