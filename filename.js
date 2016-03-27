d00ddc33db117cef767ec600434173009b425bda
Delveri Munang <chickdelveri@gmail.com>
2016-03-27 10:09:29 +0100

    Alternate function syntax

--- filename.js
+++ filename.js
@@ -1,7 +1,7 @@
 var http = require('http');
 PORT = 8888;
 
-app = http.createServer(function(req,res){
+app = http.createServer((req,res)=>{
 	res.writeHead(200,{"content-Type":"text/html",
 						'content-length':'123',
 						'connection':'keep-alive'});
@@ -12,10 +12,6 @@ app = http.createServer(function(req,res){
 	console.log("Valid request");
 
 }).listen(PORT);
-
-app.on('connect', function(req,res,head){
-	console.log("Client connected \n");
-});
 
 console.log(__filename);
 console.log("Server running on port " + PORT);
