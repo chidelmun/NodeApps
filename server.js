7ac95ccb39553094c36f7d746683c98ea998d0d9
Delveri Munang <chickdelveri@gmail.com>
2016-03-28 21:28:31 +0100

    Updated server code

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var net = require('net');
 
-serv = net.createServer((socket)=>{
-		console.log("A client Connected\n");
-
-		socket.on('data', function(data) { 
-			console.log("Got Data");
-			console.log(data.toString());
-			socket.write(data.toString());
-		});
+serv = net.createServer();
+serv.on('connection', (socket)=>{
+	console.log("A client connected\n");
+	socket.on('data', (data)=>{
+		console.log(data.toString());
+		socket.write(data.toString());
+	});
 });
+
 serv.listen(9999);
