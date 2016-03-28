fc10dc56d52db61026aec4918d7b5ba9d8540f94
Delveri Munang <chickdelveri@gmail.com>
2016-03-28 07:47:15 +0100

    Added timeout settings

--- filename.js
+++ filename.js
@@ -1,16 +1,19 @@
 var tcp = require('net');
 
 tcp.createServer((socket)=>{
-	timeout = 5000;
-	socket.setTimeout(timeout);
-	socket.on('timeout', ()=>{
-		socket.write("Connection Timeout... Bye");
-		socket.end();
-	});
-	socket.write("Welcome\n");
+	// timeout = 5000;
+	// socket.setTimeout(timeout);
+	// socket.on('timeout', ()=>{
+	// 	socket.write("Connection Timeout... Bye");
+	// 	socket.end();
+	// });
+	socket.on('connection', ()=>{
+		console.log("You are Welcome !!");
+	})
+	socket.write("You are Welcome!!\n");
 
 	socket.on('data', (data)=>{
-		console.log(data.toString());
+		socket.write("I know you are " + data);
 	})
 
 }).listen(8885);
