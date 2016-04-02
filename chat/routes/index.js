9cb15cd3b4ba3a89ba10acf069fdda17bdd189cd
Delveri Munang <chickdelveri@gmail.com>
2016-04-02 18:40:59 +0100

    Added more styles

--- chat/routes/index.js
+++ chat/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Dario' });
+  res.render('index', { title: 'Home page' });
 });
 
 module.exports = router;
