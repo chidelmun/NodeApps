09ac9d53295f873e9f1b86b0bf0e1563b1d27703
Delveri Munang <chickdelveri@gmail.com>
2016-04-05 17:57:44 +0100

    Fixed Login form

--- chat/routes/login.js
+++ chat/routes/login.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+	console.log(req.params.username);
+	if (req.body.username == 'root' && req.body.password == 'root') {
+		res.render('login', { title: 'Login' });
+	}
   res.render('login', { title: 'Login' });
 });
 
