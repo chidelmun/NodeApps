07ed835322244c33540b40f39ba3af52f1827104
Delveri Munang <chickdelveri@gmail.com>
2016-04-06 14:58:56 +0100

    Fixed server details

--- irc/index.js
+++ irc/index.js
@@ -1,5 +1,38 @@
-var config = {
-	channels: ["#davidwalshblog", "#mootools"],
-	server: "irc.freenode.net",
-	botName: "walshbot"
-};
+var irc = require('irc');
+var bot = new irc.Client('chat.freenode.net', process.env.OPENSHIFT_APP_NAME || 'CHICK_BOT', {
+    channels: ['#ublab', '#chickserver'],
+    port: 8001,
+    debug: true
+});
+
+bot.addListener("join", function(channel, who) {
+	// Welcome them in!
+	bot.say(channel, who + "...dude...welcome back!");
+});
+
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Know any good jokes?') > -1
+      || message.indexOf('good joke') > -1
+    ) {
+        bot.say(to, 'Knock knock!');
+    }
+});
+
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('who is there?') > -1
+      || message.indexOf("who's there?") > -1
+      || message.indexOf("Who's there?") > -1
+      || message.indexOf("Who is there?") > -1
+      )
+    {
+        bot.say(to, 'CHICK_BOT');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('CHICK_BOT who?') > -1
+      || message.indexOf("doris who?") > -1
+     )
+    {
+        bot.say(to, "CHICK_BOT locked, that's why I'm knocking!");
+    }
+});
